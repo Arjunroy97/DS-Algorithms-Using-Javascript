fede4c0cb5d710e9c8a3a2fefd906073b533de2c
Arjun-Roy <mearjun1997roy@gmail.com>
2022-02-14 12:37:03 +0600

    unupuevalue file add comment

--- uniquevalue.js
+++ uniquevalue.js
@@ -1,5 +1,10 @@
 //             i j                                             
 // conditon : [1,1,2,2,3,3,4,4,5,5,6,6,7,7]
+/** 
+ * থরি i = 0; এবং j = 1;
+ * এখনে condition হল যদি i ও j এর মান সমান থাকে তাহলে j++ হবে
+ * আর যদি i ও j এর মান সমান না থাকে তাহলে i++ হবে এবং j এর মান i এর মানে বসবে a.x : arr[i] = arr[j]
+ */
 
 function countUniqueValue(arr){
     if(arr.length > 0){
