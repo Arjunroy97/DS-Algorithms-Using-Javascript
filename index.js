8f145fc9cc89ebe8951e6e23747660ed8a9add9a
Arjun-Roy <mearjun1997roy@gmail.com>
2022-02-14 11:18:15 +0600

    cupple of first number sumation of zero

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("hello")
+console.clear();
+/** Checking of number first Cupple of sum Zero*/
+function cuppleOfSumZero(arr){
+    for(let num of arr){
+       for(let j = 1; j < arr.length;j++){
+           if(num + arr[j] === 0){
+               return [num, arr[j]]
+           }
+       }
+    }
+}
+
+// const cosz = cuppleOfSumZero([-5,-4,-3,-2,0,2,4,6,8])
+// console.log(cosz);
+
+
+// 2nd method
+function sumOfPair(arr){
+    let left = 0;
+    let right = arr.length-1;
+    while(left < right){
+        let sum = arr[left]+ arr[right];
+        if(sum === 0){
+            return [arr[left], arr[right]]
+        }else if(sum > 0){
+            right--
+        }else{
+            left++
+        }
+    }
+}
+
+const cosz = sumOfPair([-5,-4,-3,-2,0,2,4,6,8])
+// console.log(cosz);
